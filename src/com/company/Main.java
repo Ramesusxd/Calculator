9e74b1a3c13fc8e9dd8991e3528248a92345385f
RamesusXD <j1gamer4life@gmail.com>
2020-12-09 12:09:23 -0800

    A command line calculator I programmed in Java just to practice

---
+++ src/com/company/Main.java
@@ -0,0 +1,47 @@
+package com.company;
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+	    char operator;
+	    double num1,num2;
+	    Scanner input = new Scanner(System.in);
+
+	    System.out.println("Enter the Operator ( +,-,*,/ )");
+	    operator = input.next().charAt(0);
+
+	    System.out.println("Enter the two numbers one by one");
+	    num1 = input.nextDouble();
+        num2 = input.nextDouble();
+
+        input.close();
+
+        switch( operator ) {
+            case '+':
+                System.out.printf("%f + %f = %f", num1,num2,(num1+num2));
+                break;
+
+            case '-':
+                System.out.printf("%f - %f = %f", num1,num2,(num1-num2));
+                break;
+
+            case '*':
+                System.out.printf("%f * %f = %f", num1,num2,(num1*num2));
+                break;
+
+            case '/':
+                if( num2 != 0 )
+                    System.out.printf("%f / %f = %f", num1,num2,(num1/num2));
+                else
+                    System.out.println("Divide by Zero Situation");
+                break;
+
+             default:
+                 System.out.printf("%c is an invalid operator",operator);
+
+
+        }
+    }
+}
